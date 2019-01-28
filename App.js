1aa6ec28dae21ed6dca58d5d87eebcc36011b138
Anders Lundback <anders.lundback@postoffice.co.uk>
2019-01-28 12:37:45 +0000

    Initial commit from Create React App

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="info">
+        <h1>Github users</h1>
+        <img src="http://placehold.it/75" />
+        <div>
+          <div>Name here...</div>
+          <div>Company Name here...</div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
